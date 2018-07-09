420d0b69505063fcbeeffdabf759b19083cf7c2e
Ubuntu <ubuntu@jumpbox.c.ps-amcginlay.internal>
2018-07-09 20:11:55 +0000

    bring back region and zones

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -11,6 +11,11 @@ PCF_PIVNET_UAA_TOKEN=CHANGE_ME_PCF_PIVNET_UAA_TOKEN   # see https://network.pivo
 PCF_DOMAIN_NAME=CHANGE_ME_DOMAIN_NAME                 # e.g. pivotaledu.io
 PCF_SUBDOMAIN_NAME=CHANGE_ME_SUBDOMAIN_NAME           # e.g. cls99env66
 PCF_OPSMAN_ADMIN_PASSWD=CHANGE_ME_OPSMAN_ADMIN_PASSWD # e.g. for simplicity, recycle your PCF_PIVNET_UAA_TOKEN 
+PCF_REGION=CHANGE_ME_REGION                           # e.g. us-central1	
+PCF_AZ_1=CHANGE_ME_AZ_1                               # e.g. us-central1-a	
+PCF_AZ_2=CHANGE_ME_AZ_2                               # e.g. us-central1-b	
+PCF_AZ_3=CHANGE_ME_AZ_3                               # e.g. us-central1-c	
+
 
 PCF_OPSMAN_IMAGE=ops-manager-us/pcf-gcp-2.1-build.214.tar.gz # PLEASE DON'T CHANGE ME!
 PCF_OPSMAN_FQDN=pcf.\${PCF_SUBDOMAIN_NAME}.\${PCF_DOMAIN_NAME}
