e2e3d4b0ba30755aa062d4cdc6bf365251f0aa62
Alan McGinlay <amcginlay@pivotal.io>
2018-11-30 09:27:43 +0000

    Getting SCS configured for MySQL v2 tile

--- config/p-spring-cloud-services/1.5/properties.json.sh
+++ config/p-spring-cloud-services/1.5/properties.json.sh
@@ -1,3 +1,10 @@
 cat <<EOF
-{}
+{
+  ".deploy-service-broker.persistence_store_service": {
+    "value": "p.mysql"
+  },
+  ".deploy-service-broker.persistence_store_service_plan": {
+    "value": "db-small"
+  }
+}
 EOF
